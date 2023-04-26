19dbd6d8265eea91c3c508a77d2edbfe7151b5c0
Fasasi Hafis <125377737+harhfeez@users.noreply.github.com>
2023-04-26 16:07:26 +0100

    Delete calculate.py

--- calculate.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import Qtwidgets
-from Ui_calculator import Ui_calculator
-class Calculatorwindow(Qtwidgets.QMainwindow,Ui_calculator):
-    def __init__(self):
-        super().__init__()
-        self.setupui(self)
-        self.show()
